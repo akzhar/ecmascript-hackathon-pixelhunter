4123248d69ef5b941fa0a225be255817ce741cc3
Akzhar <akzharik@mail.ru>
2020-04-21 00:24:32 +0300

    task 4.1

--- js/main.js
+++ js/main.js
@@ -1,22 +1,7 @@
-import switchScreen from './screen.js';
+import {renderScreen, renderLives, renderStats, renderResults} from './render.js';
+import screens from './screens/screens.js';
+
 import data from './data/data.js';
-
-import intro from './screens/intro.js';
-import greeting from './screens/greeting.js';
-import rules from './screens/rules.js';
-import game from './screens/game.js';
-import stats from './screens/stats/stats.js';
-
-const screens =
-[
-  {screen: intro},
-  {screen: greeting},
-  {screen: rules},
-  {screen: game.getScreen(data, 2), questions: 2},
-  {screen: game.getScreen(data, 1), questions: 1},
-  {screen: game.getScreen(data, 3), questions: 3},
-  {screen: stats}
-];
 
 let current = 0;
 
@@ -24,57 +9,107 @@ restartGame();
 
 function restartGame() {
   current = 0;
-  switchScreen(screens[current].screen);
+  dropGameData();
+  changeScreen();
+}
+
+function continueGame() {
+  current++;
+  changeScreen();
+}
+
+function endGame() {
+  current = screens.length - 1;
+  changeScreen();
+}
+
+function changeScreen() {
+  renderScreen(screens[current].screen);
   onScreenChange();
 }
 
-function nextScreen() {
-  current++;
-  switchScreen(screens[current].screen);
-  onScreenChange();
+function dropGameData() {
+  data.user = ``;
+  data.lives = 3;
+  data.answers = [];
 }
 
 function onScreenChange() {
-  const screen = screens[current].screen;
-  const questions = screens[current].questions;
-  if (screen === intro) {
+  const screenName = screens[current].name;
+
+  if (screenName === `intro`) {
     const asterisk = document.querySelector(`.intro__asterisk`);
-    asterisk.addEventListener(`click`, nextScreen);
+    asterisk.addEventListener(`click`, continueGame);
   }
-  if (screen === greeting) {
+  if (screenName === `greeting`) {
     const startArrow = document.querySelector(`.greeting__continue`);
-    startArrow.addEventListener(`click`, nextScreen);
+    startArrow.addEventListener(`click`, continueGame);
   }
-  if (screen !== intro && screen !== greeting) {
+  if (screenName !== `intro` && screenName !== `greeting`) {
     const backArrow = document.querySelector(`.back`);
     backArrow.addEventListener(`click`, restartGame);
   }
-  if (screen === rules) {
+  if (screenName === `rules`) {
     const nameInput = document.querySelector(`.rules__input`);
-    const submitBtn = document.querySelector(`.rules__button`);
+    const startBtn = document.querySelector(`.rules__button`);
     nameInput.addEventListener(`input`, () => {
-      submitBtn.disabled = (nameInput.value === ``) ? true : false;
+      startBtn.disabled = (nameInput.value === ``) ? true : false;
     });
-    submitBtn.addEventListener(`click`, nextScreen);
+    startBtn.addEventListener(`click`, onStartBtnClick);
   }
-  if (questions) {
-    const selector = (questions === 3) ? `.game__option` : `.game__answer > input`;
-    const answers = document.querySelectorAll(selector);
-    answers.forEach((answer) => answer.addEventListener(`click`, onAnswer));
+  if (screenName === `game`) {
+    const gameType = screens[current].gameType;
+    renderLives(data.lives);
+    renderStats(data.answers);
+    const selector = (gameType === 3) ? `.game__option` : `.game__answer > input`;
+    const answersElem = document.querySelectorAll(selector);
+    answersElem.forEach((answer) => answer.addEventListener(`click`, onEachAnswer));
+  }
+  if (screenName === `stats`) {
+    renderResults(data.answers, data.lives);
   }
 }
 
-function onAnswer() {
-  // запись ответов если это вопрос
-  if (screens[current].questions !== 3) {
-    // console.log(current - 2 + `: you think this is a ` + this.value);
+function onStartBtnClick() {
+  const nameInput = document.querySelector(`.rules__input`);
+  data.user = nameInput.value.trim();
+  continueGame();
+}
+
+function onEachAnswer(evt) {
+  if (screens[current].gameType !== 3) {
     if (isAllAnswersGiven()) {
-      nextScreen();
+      const isRight = isAllAnswersGivenRight();
+      onValidAnswer(isRight);
     }
   } else {
-    // console.log(current - 2 + `: you think this is a paint`);
-    nextScreen();
+    const isRight = evt.currentTarget.dataset.answer === evt.currentTarget.dataset.value;
+    onValidAnswer(isRight);
   }
+}
+
+function onValidAnswer(isRight) {
+  saveAnswer(isRight);
+  if (!isRight) {
+    data.lives--;
+  }
+  const nextAction = (data.lives >= 0) ? continueGame : endGame;
+  nextAction();
+}
+
+function saveAnswer(isRight) {
+  data.answers.push({isOK: isRight, time: 15});
+}
+
+function isAllAnswersGivenRight() {
+  const options = Array.from(document.querySelectorAll(`.game__option`));
+  return options.every((option) => {
+    const answers = Array.from(option.querySelectorAll(`.game__answer`));
+    return answers.some((answer) => {
+      const input = answer.querySelector(`input`);
+      return input.checked && input.dataset.answer === input.value;
+    });
+  });
 }
 
 function isAllAnswersGiven() {
--- js/screens/game.js
+++ js/screens/game.js
@@ -1,105 +1,83 @@
-import utils from '../utils.js';
+import {isPhoto, isPaint, isRight} from '../debug.js';
 
-function getHeader(time, lives) {
-  return `<header class="header">
-    <button class="back">
-      <span class="visually-hidden">Вернуться к началу</span>
-      <svg class="icon" width="45" height="45" viewBox="0 0 45 45" fill="#000000">
-        <use xlink:href="img/sprite.svg#arrow-left"></use>
-      </svg>
-      <svg class="icon" width="101" height="44" viewBox="0 0 101 44" fill="#000000">
-        <use xlink:href="img/sprite.svg#logo-small"></use>
-      </svg>
-    </button>
-    <div class="game__timer">${time}</div>
-    <div class="game__lives">
-      ${utils.getLives(lives)}
-    </div>
-  </header>`;
-}
-
-function getGameContent(game, questions) {
+function getGameContent(game, gameType) {
   let content = ``;
-  if (questions === 2) {
+  if (gameType === 2) {
     content = `<form class="game__content">
       <div class="game__option">
-        <img src="${game.option1}" alt="Option 1" width="468" height="458">
+        <img src="${game.questions[0].src}" alt="Option 1" width="468" height="458">
         <label class="game__answer game__answer--photo">
-          <input class="visually-hidden" name="question1" type="radio" value="photo">
-          <span>Фото</span>
+          <input class="visually-hidden" name="question1" type="radio" value="photo" data-answer="${game.questions[0].answer}">
+          <span ${isPhoto(game.questions[0].answer)}>Фото</span>
         </label>
         <label class="game__answer game__answer--paint">
-          <input class="visually-hidden" name="question1" type="radio" value="paint">
-          <span>Рисунок</span>
+          <input class="visually-hidden" name="question1" type="radio" value="paint" data-answer="${game.questions[0].answer}">
+          <span ${isPaint(game.questions[0].answer)}>Рисунок</span>
         </label>
       </div>
       <div class="game__option">
-        <img src="${game.option2}" alt="Option 2" width="468" height="458">
+        <img src="${game.questions[1].src}" alt="Option 2" width="468" height="458">
         <label class="game__answer  game__answer--photo">
-          <input class="visually-hidden" name="question2" type="radio" value="photo">
-          <span>Фото</span>
+          <input class="visually-hidden" name="question2" type="radio" value="photo" data-answer="${game.questions[1].answer}">
+          <span ${isPhoto(game.questions[1].answer)}>Фото</span>
         </label>
         <label class="game__answer  game__answer--paint">
-          <input class="visually-hidden" name="question2" type="radio" value="paint">
-          <span>Рисунок</span>
+          <input class="visually-hidden" name="question2" type="radio" value="paint" data-answer="${game.questions[1].answer}">
+          <span ${isPaint(game.questions[1].answer)}>Рисунок</span>
         </label>
       </div>
     </form>`;
-  } else if (questions === 1) {
+  } else if (gameType === 1) {
     content = `<form class="game__content  game__content--wide">
       <div class="game__option">
-        <img src="${game.option1}" alt="Option 1" width="705" height="455">
+        <img src="${game.question.src}" alt="Option 1" width="705" height="455">
         <label class="game__answer  game__answer--photo">
-          <input class="visually-hidden" name="question1" type="radio" value="photo">
-          <span>Фото</span>
+          <input class="visually-hidden" name="question1" type="radio" value="photo" data-answer="${game.question.answer}">
+          <span ${isPhoto(game.question.answer)}>Фото</span>
         </label>
         <label class="game__answer  game__answer--paint">
-          <input class="visually-hidden" name="question1" type="radio" value="paint">
-          <span>Рисунок</span>
+          <input class="visually-hidden" name="question1" type="radio" value="paint" data-answer="${game.question.answer}">
+          <span ${isPaint(game.question.answer)}>Рисунок</span>
         </label>
       </div>
     </form>`;
-  } else if (questions === 3) {
+  } else if (gameType === 3) {
     content = `<form class="game__content  game__content--triple">
-      <div class="game__option">
-        <img src="${game.option1}" alt="Option 1" width="304" height="455">
+      <div class="game__option" data-value="0" data-answer="${game.question.answer}" ${isRight(game.question.answer === 0)}>
+        <img src="${game.question.src[0]}" alt="Option 1" width="304" height="455">
       </div>
-      <div class="game__option game__option--selected">
-        <img src="${game.option2}" alt="Option 2" width="304" height="455">
+      <div class="game__option" data-value="1" data-answer="${game.question.answer}" ${isRight(game.question.answer === 1)}>
+        <img src="${game.question.src[1]}" alt="Option 2" width="304" height="455">
       </div>
-      <div class="game__option">
-        <img src="${game.option3}" alt="Option 3" width="304" height="455">
+      <div class="game__option" data-value="2" data-answer="${game.question.answer}" ${isRight(game.question.answer === 2)}>
+        <img src="${game.question.src[2]}" alt="Option 3" width="304" height="455">
       </div>
     </form>`;
   }
   return content;
 }
 
-function getStats() {
-  return `<ul class="stats">
-    <li class="stats__result stats__result--wrong"></li>
-    <li class="stats__result stats__result--slow"></li>
-    <li class="stats__result stats__result--fast"></li>
-    <li class="stats__result stats__result--correct"></li>
-    <li class="stats__result stats__result--wrong"></li>
-    <li class="stats__result stats__result--unknown"></li>
-    <li class="stats__result stats__result--slow"></li>
-    <li class="stats__result stats__result--unknown"></li>
-    <li class="stats__result stats__result--fast"></li>
-    <li class="stats__result stats__result--unknown"></li>
-  </ul>`;
-}
-
-function getScreen(data, questions) {
-  const game = data.games[questions];
+function getGame(game) {
   return `<div id="main" class="central__content">
-    ${getHeader(data.time, data.lives)}
+    <header class="header">
+      <button class="back">
+        <span class="visually-hidden">Вернуться к началу</span>
+        <svg class="icon" width="45" height="45" viewBox="0 0 45 45" fill="#000000">
+          <use xlink:href="img/sprite.svg#arrow-left"></use>
+        </svg>
+        <svg class="icon" width="101" height="44" viewBox="0 0 101 44" fill="#000000">
+          <use xlink:href="img/sprite.svg#logo-small"></use>
+        </svg>
+      </button>
+      <div class="game__timer">3:00</div>
+      <div class="game__lives"></div>
+    </header>
     <section class="game">
       <p class="game__task">${game.task}</p>
-      ${getGameContent(game, questions)}
-      ${getStats(data.stats)}
+      ${getGameContent(game, game.gameType)}
+      <ul class="stats"></ul>
     </section>
   </div>`;
 }
 
-export default {getScreen};
+export default getGame;
